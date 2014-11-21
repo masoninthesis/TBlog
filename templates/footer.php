926ddea7c555d41fdc64d4f9d0fd1e0353f6c4b2
Mason L <mason.lawlor@me.com>
2014-11-20 18:20:57 -0800

    mobile styled. still needs hamburger

--- templates/footer.php
+++ templates/footer.php
@@ -17,8 +17,11 @@
         <a href="https://twitter.com/timelyapp" class="twitter-follow-button" data-show-count="false">Follow @timelyapp</a>
         <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
       </li>
-      <li>
-      <div class="fb-like" data-href="https://www.facebook.com/TimelyApp" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+      <li class="fb-tablet">
+        <div class="fb-like" data-href="https://www.facebook.com/TimelyApp" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+      </li>
+      <li class="fb-phone">
+      <div class="fb-like" data-href="https://www.facebook.com/TimelyApp" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
       </li>
     </ul>
   </div>
@@ -38,7 +41,6 @@
             <li><a href="http://timelyapp.com/about">Company</a></li>
           </ul>
         </div>
-        <hr>
       </div>
       <div class="copyright text-center">
         <p>Copyright &copy; <?php echo date("Y") ?> Mathias Mikkelsen AS. All rights reserved.</p>
